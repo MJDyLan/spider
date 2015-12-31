f413229a690c8103f264398b54863903ca71d955
邱吉胜 <jisheng_qiu@kingdee.com>
2015-12-31 17:08:21 +0800

    'https'

--- src/main/java/com/zimu/javacore/security/HttpUtils.java
+++ src/main/java/com/zimu/javacore/security/HttpUtils.java
@@ -6,45 +6,32 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.UUID;
+import java.security.SecureRandom;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
 
 public class HttpUtils {
 	public static String sendPost(String path,String params) throws UnsupportedEncodingException, IOException{
 		String encoding="UTF-8";
 		byte[] data = params.getBytes(encoding);
         URL url =new URL(path);
-        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
+        buildHttpsURLConnection(conn);
         conn.setRequestMethod("POST");
         conn.setDoOutput(true);
         //application/x-javascript text/xml->xml数据 application/x-javascript->json对象 application/x-www-form-urlencoded->表单数据
-        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset="+ encoding);
-        conn.setRequestProperty("Content-Length", String.valueOf(data.length));
-        
-        conn.setRequestProperty("networkType", UUID.randomUUID().toString());
-
-        conn.setRequestProperty("location", "");
-        
-        conn.setRequestProperty("operateId", UUID.randomUUID().toString());
-
-        conn.setRequestProperty("screenSize", "480X854");
-
-        conn.setRequestProperty("clientVersion", "2.6.0");
-
-        conn.setRequestProperty("deviceId", "866977025217383");
-
-        conn.setRequestProperty("clientDatetime", "2015-12-29 23:53:52.642");
-
-        conn.setRequestProperty("clientType", "4.4.4");
-
-        conn.setRequestProperty("channelId", "4C9EF1676B564240DF6AA684F968E85A");
-
-        conn.setRequestProperty("tokenId", "");
-
-        conn.setRequestProperty("clientIp", "192.168.1.3");
-        
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Accept-Charset", "utf-8");
+        conn.setRequestProperty("Connection", "Keep-Alive");
+        conn.setRequestProperty("User-Agent", "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586");
         conn.setConnectTimeout(5*1000);
+        conn.setReadTimeout(5*1000);
         OutputStream outStream = conn.getOutputStream();
         outStream.write(data);
         outStream.flush();
@@ -67,5 +54,23 @@ public class HttpUtils {
 		    }
 		    byte[] buffer =  baos.toByteArray();
 		    return buffer;
-		  }
+     }
+	 /**
+		 * 设置https相关属性
+		 */
+		private static void buildHttpsURLConnection(HttpsURLConnection connection) {
+			try {
+				SSLContext ctx = SSLContext.getInstance("TLS");
+				ctx.init(new KeyManager[0], new TrustManager[]{new DefaultTrustManager()}, new SecureRandom());
+				SSLContext.setDefault(ctx);
+				connection.setHostnameVerifier(new HostnameVerifier() {
+					public boolean verify(String hostname, SSLSession session) {
+						return true;
+					}
+				});
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
 }
