14516598392f2198eb391aa247f39c256fc3c719
邱吉胜 <jisheng_qiu@kingdee.com>
2016-01-08 17:46:46 +0800

    add json utils

---
+++ src/main/java/com/zimu/javacore/utils/KsonUtils.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.zimu.javacore.utils;
+
+/**
+ * @author jasonChiu
+ * 
+ * @title json字符串处理类，自己开发吧
+ * 
+ * @time 2016年1月8日下午4:05:48
+ * @version 1.0
+ */
+public class KsonUtils {
+	
+
+}
--- src/main/java/com/zimu/spider/yirendai/model/UserModel.java
+++ src/main/java/com/zimu/spider/yirendai/model/UserModel.java
@@ -4,5 +4,29 @@ public class UserModel {
 	private String account;
 	private String password;
 	
+	private String token;
 
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}	
 }
