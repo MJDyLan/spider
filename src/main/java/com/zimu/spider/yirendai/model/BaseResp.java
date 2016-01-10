ddcb0e5f21d6be81e3ce9e0315a923599cc5d98b
jishengqiu <jisheng_qiu@kingdee.com>
2016-01-11 01:07:29 +0800

    准备对响应的接口数据做json处理，有点累了，明天继续。

---
+++ src/main/java/com/zimu/spider/yirendai/model/BaseResp.java
@@ -0,0 +1,44 @@
+package com.zimu.spider.yirendai.model;
+/**
+ * 
+ * @title 宜人贷抓取接口响应的公共字段
+ * 
+ * @author JasonChiu
+ * @time 2016年1月11日上午12:55:16
+ * @version 1.0
+ */
+public class BaseResp {
+	
+	private String errorCode;
+	private String msg;
+	private String result;
+
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public String toString() {
+		return "BaseResp [result=" + this.result + ", errorCode="
+				+ this.errorCode + ", msg=" + this.msg + "]";
+	}
+}
