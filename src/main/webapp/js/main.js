74930af7e8190313be397212a88fab7fd95938ec
邱吉胜 <jisheng_qiu@kingdee.com>
2016-03-11 15:37:11 +0800

    js study

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -7,3 +7,15 @@ require.config({
 			"aa":"js/aa"
 	}
 });
+
+/*require(["math2"],function(a){
+	alert(a.add(10,5));
+	alert(a.plus(10,5));
+	alert(a.multi(10,5));
+	alert(a.divide(10,5));
+});*/
+require(["math"],function(a){
+	alert(a.add(10,5));
+	alert(a.kk(10,5));
+
+});
